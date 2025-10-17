c0ad56c2c69321b6c95142f457d85c0620701406
Sayuri <125521143+StefannySayuri@users.noreply.github.com>
2025-10-16 21:07:08 -0300

    Delete sw/faq.js

--- sw/faq.js
+++
@@ -1,8 +0,0 @@
-function toggleAnswer(faqId) {
-        var answer = document.getElementById(faqId);
-        if (answer.style.display === "block") {
-            answer.style.display = "none";
-        } else {
-            answer.style.display = "block";
-        }
-    }
